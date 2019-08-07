51a9fa272d2c136ac03c2cb33d54507b80ddc900
ZhenqiSong <songzhenqi92@gmail.com>
2019-08-07 23:16:49 +0800

    添加了string

--- chapter2/1_string.cpp
+++ chapter2/1_string.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 
 int main() {
-	cout << "hello, world" << endl;
+	
+	// string 的初始化
+	string s1;
+	string s2 = s1;
+	string s3 = "hiye"; // 除了最后的空字符串，都拷贝到了s3中
+	string s4(10, 'c'); // 类似与python 中的 "c" * 10
+
+	cout << s4 << endl;
+
+
+
 	return 0;
 }
